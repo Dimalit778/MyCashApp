c6d1ebca1f803ee0524ce9b092cf25326c05ea61
Dimalit778 <119805398+Dimalit778@users.noreply.github.com>
2025-04-17 20:27:15 +0300

    NEW---

--- backend/server.js
+++ backend/server.js
@@ -31,7 +31,8 @@ app.use(bodyParser.json());
 // --- CORS configurations
 app.use(
   cors({
-    origin: [process.env.CLIENT_URL, process.env.RENDER_FRONTEND_URL],
+    // origin: [process.env.CLIENT_URL, process.env.RENDER_FRONTEND_URL],
+    origin: ["http://localhost:3000", "https://mycashfrontend.onrender.com"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   })
